7cac5ab1d2812e83fa56a550af6744995acc61e3
Edithson <edithson@Mac-de-Edithson.local>
2024-10-14 14:24:22 -0400

    New changes october 14th

--- templates/sections/vehiculosEntrada.php
+++ templates/sections/vehiculosEntrada.php
@@ -64,7 +64,6 @@
                         </header>
 
                         <div class="grid md:grid-cols-2 gap-6 mb-6">
-                            <!-- Columna izquierda: Formulario de entrada -->
                             <div class="bg-gray-50 dark:bg-gray-700 p-6 rounded-lg shadow">
                                 <form id="entryForm" class="space-y-4">
                                     <div class="relative">
@@ -107,13 +106,13 @@
                                         <label for="entryWeight" class="block text-sm font-medium text-gray-700 dark:text-gray-300 mb-1">Peso de Entrada</label>
                                         <div class="flex">
                                             <input type="number" name="entryWeight" id="entryWeight" step="0.01" class="rounded-l-lg bg-gray-50 border border-gray-300 text-gray-900 focus:ring-blue-500 focus:border-blue-500 block flex-1 min-w-0 w-full text-sm p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500" required>
-                                            <button type="button" class="inline-flex items-center px-3 text-sm font-medium text-white bg-blue-700 rounded-r-md border border-l-0 border-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800">
+                                            <button type="button" class="inline-flex items-center px-3 text-sm font-medium text-white bg-blue-900 rounded-r-md border border-l-0 border-blue-600 hover:bg-blue-600 focus:ring-4 focus:outline-none focus:ring-blue-300 dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800">
                                                 Leer peso
                                             </button>
                                         </div>
                                     </div>
                                     <div class="text-center">
-                                        <button type="submit" class="inline-flex justify-center py-2 px-4 border border-transparent shadow-sm text-sm font-medium rounded-md text-white bg-blue-600 hover:bg-blue-700 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-blue-500">
+                                        <button type="submit" class="inline-flex justify-center py-2 px-4 border border-transparent shadow-sm text-sm font-medium rounded-md text-white bg-blue-900 hover:bg-blue-600 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-blue-500">
                                             Registrar
                                         </button>
                                     </div>
