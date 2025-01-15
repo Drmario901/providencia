a94cf692410b2da3d163fc6725c82f1dcbefe952
Edithson <edithson@zohomail.com>
2025-01-15 14:34:03 -0400

    Test

--- php/logout.php
+++ php/logout.php
@@ -1,15 +1,21 @@
 <?php
-    header("Access-Control-Allow-Origin: http://serviaves.42web.io");
-    header("Access-Control-Allow-Methods: GET, POST, PUT, DELETE, OPTIONS");
-    header("Access-Control-Allow-Headers: Content-Type, Authorization");
-    
-    require 'global.php';
-    
-    session_start();
-    session_unset();
-    session_write_close();
+header("Access-Control-Allow-Origin: *");
+header("Access-Control-Allow-Methods: GET, POST, PUT, DELETE, OPTIONS");
+header("Access-Control-Allow-Headers: Content-Type, Authorization");
 
-    $fullUrl = isset($_POST['fullUrl']) ? $_POST['fullUrl'] : '';
-    header("Location: " . $fullUrl);
+if ($_SERVER['REQUEST_METHOD'] === 'OPTIONS') {
+    http_response_code(200);
+    exit();
+}
 
-?>
+require 'global.php';
+
+session_start();
+session_unset();
+session_write_close();
+
+$fullUrl = isset($_POST['fullUrl']) ? $_POST['fullUrl'] : '';
+echo json_encode([
+    'success' => true,
+    'redirectUrl' => $fullUrl,
+]);
